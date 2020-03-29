87b5ed90f2c23336d3557dadddb77302bc4576a0
Tú Đình <kemdanhrang@gmail.com>
2020-03-29 16:10:40 +0700

    Initial Commit

---
+++ kite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kite
+//
+//  Created by Tú Đình on 3/29/20.
+//  Copyright © 2020 Tú Đình. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
